f8091cdf58d0e4254d6cd399369d4bee374e7fbc
MarwanHimeur <marwan.himeur@iut-dijon.u-bourgogne.fr>
2025-11-03 18:10:50 +0100

    Ajustements et commentaires

--- EpicurApp/EpicurAppData/Repositories/ClientRepositories.cs
+++ EpicurApp/EpicurAppData/Repositories/ClientRepositories.cs
@@ -7,6 +7,9 @@ using Microsoft.Data.Sqlite;
 
 namespace EpicurAppData.Repositories
 {
+    /// <summary>
+    /// Implémentation du repository client utilisant SQLite.
+    /// </summary>
     public class ClientRepository : IClientRepository
     {
         private string _connectionString = "Data Source=epicurapp.db";
--- EpicurApp/EpicurAppLogic/Services/IClientService.cs
+++ EpicurApp/EpicurAppLogic/Services/IClientService.cs
@@ -7,8 +7,19 @@ using EpicurAppData;
 
 namespace EpicurAppLogic.Services
 {
+    /// <summary>
+    /// Interface pour la gestion des clients.
+    /// Définit les opérations disponibles sur les clients.
+    /// </summary>
     public interface IClientService
     {
+        /// <summary>
+        /// Ajoute un nouveau client à la base.
+        /// </summary>
+        /// <param name="client">Le client à ajouter.</param>
+        /// <exception cref="EpicurAppData.Exceptions.InvalidFieldException">
+        /// Levée si un champ renseigné est invalide.
+        /// </exception>
         void AjouterClient(Client client);
     }
 }
