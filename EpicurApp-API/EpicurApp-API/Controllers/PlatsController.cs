263fd9964af09a2bb3dc5c7d865b03f10f68a75e
LouisPage14 <louis.page@iut-dijon.u-bourgogne.fr>
2025-11-04 16:14:27 +0100

    API Get plats

---
+++ EpicurApp-API/EpicurApp-API/Controllers/PlatsController.cs
@@ -0,0 +1,36 @@
+﻿using EpicurApp_API.DAO;
+using EpicurApp_API.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace EpicurApp_API.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class PlatsController : ControllerBase
+    {
+        private readonly IPlatDAO platDAO;
+        private readonly ILogger<PlatsController> logger;
+
+        public PlatsController(IPlatDAO platDAO, ILogger<PlatsController> logger)
+        {
+            this.platDAO=platDAO;
+            this.logger=logger;
+        }
+
+        //GET: api/plats
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Plat>>> GetAllPlats()
+        {
+            try
+            {
+                var plats = await platDAO.GetAllAsync();
+
+                return Ok(plats);
+            }
+            catch (Exception e)
+            {
+                return StatusCode(500, $"Erreur interne du serveur lors de l'accès aux données : {e.Message}");
+            }
+        }
+    }
+}
--- EpicurApp-API/EpicurApp-API/Program.cs
+++ EpicurApp-API/EpicurApp-API/Program.cs
@@ -1,7 +1,9 @@
+using EpicurApp_API.DAO;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-
+builder.Services.AddScoped<IPlatDAO, PlatDAO>();
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
