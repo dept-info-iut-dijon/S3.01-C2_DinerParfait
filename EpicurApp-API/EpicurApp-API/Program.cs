30926888d467a58de053bc415d5f5ed370e4f4f6
Romain SOUVETON <romain.souveton@iut-dijon.u-bourgogne.fr>
2025-11-04 17:12:03 +0100

    Revert "Merge branch 'US1.2' into Formulaire-IHM-création-client"
    This reverts commit f81caaeb73c499d919f9bf2a85b70526f63aab6d, reversing
    changes made to 11cff173eff958cc792aa022ba1f3e02d4346760.

--- EpicurApp-API/EpicurApp-API/Program.cs
+++ EpicurApp-API/EpicurApp-API/Program.cs
@@ -1,6 +1,3 @@
-using EpicurApp_API.DAO;
-using EpicurAPP_Partage.Interfaces;
-
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -9,8 +6,6 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
-
-builder.Services.AddScoped<IClientDAO, ClientDAO>();
 
 var app = builder.Build();
 
