ad854a60aa1500894bcb39f66f1096e21acfdf3f
MarwanHimeur <marwan.himeur@iut-dijon.u-bourgogne.fr>
2025-11-04 09:29:34 +0100

    implémentation DAO et rectification de l'architecture

---
+++ EpicurApp-API/EpicurApp-API/DAO/IClientDAO.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using EpicurApp_API.Models;
+
+namespace EpicurApp_API.DAO
+{
+    /// <summary>
+    /// Interface pour accéder à la base de données des clients.
+    /// </summary>
+    public interface IClientRepository
+    {
+        /// <summary>
+        /// Ajoute un client dans la base de données.
+        /// </summary>
+        /// <param name="client">Le client à ajouter.</param>
+        void AjouterClient(Client client);
+    }
+}
---
+++ EpicurApp-API/EpicurApp-API/Models/Client.cs
@@ -0,0 +1,28 @@
+﻿namespace EpicurApp_API.Models
+{
+    /// <summary>
+    /// Représente un client.
+    /// Contient les information sur le client
+    /// </summary>
+    public class Client
+    {
+        //Nom du client
+        public string Nom { get; set; }
+
+        //Prenom du client
+        public string Prenom { get; set; }
+
+        //Numéro de téléphone du client
+        public string Telephone { get; set; }
+
+        //Email du client
+        public string Email { get; set; }
+
+        //Allergies du client
+        public string Allergies { get; set; }
+
+        //Note supplementaire a propos du client (Préféreces, ...)
+        public string Note { get; set; }
+    }
+}
+
