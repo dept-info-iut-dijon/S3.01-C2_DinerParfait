f6287e21752e4c788352f5dc1eb6bc7bb5bdba0c
Louis PAGE <lp519536@iut21.u-bourgogne.fr>
2025-11-04 11:20:22 +0100

    US2.1 Classes Menu et plat

---
+++ EpicurApp-API/EpicurAppData/Models/Client.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EpicurAppData.Models
+{
+    public class Client
+    {
+        public int Id { get; set; }
+        public string Nom { get; set; }
+        public string Prenom { get; set; }
+        public string Telephone { get; set; }
+        public string Email { get; set; }
+        public string Allergies { get; set; }
+        public string Note { get; set; }
+    }
+}
