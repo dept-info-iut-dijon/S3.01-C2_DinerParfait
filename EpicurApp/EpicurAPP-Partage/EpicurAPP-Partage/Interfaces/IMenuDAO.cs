463a23436b578d2321bdec5613404efc92deeda9
MarwanHimeur <marwan.himeur@iut-dijon.u-bourgogne.fr>
2025-11-05 12:11:40 +0100

    ajout des interface menuDAO et MenuService

---
+++ EpicurApp/EpicurAPP-Partage/EpicurAPP-Partage/Interfaces/IMenuDAO.cs
@@ -0,0 +1,14 @@
+﻿using EpicurApp_API.Models;
+using System.Collections.Generic;
+
+namespace EpicurAPP_Partage.Interfaces
+{
+    public interface IMenuDAO
+    {
+        void AjouterMenu(Menu menu);
+        Menu? GetById(int id);
+        List<Menu> GetAll();
+        void AjouterPlatsAuMenu(int menuId, List<int> platIds);
+    }
+}
+
