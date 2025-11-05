050da60d107566b05b6a856453e298f406ada734
MarwanHimeur <marwan.himeur@iut-dijon.u-bourgogne.fr>
2025-11-05 10:35:22 +0100

    suppression du dossier data

---
+++ EpicurApp/EpicurAPP-Partage/EpicurAPP-Partage/Models/Menu.cs
@@ -0,0 +1,14 @@
+﻿namespace EpicurApp_API.Models
+{
+    public class Menu
+    {
+        public int Id { get; set; }
+        public string Nom {  get; set; }
+        public DateTime Date { get; set; }
+        public string Statut { get; set; } = "Brouillon";
+        public decimal CoutGlobal { get; set; }
+        public int TempsPreparationMinutes { get; set; }
+
+        public ICollection<Plat> PlatsAssocies { get; set; }=new List<Plat>();
+    }
+}
