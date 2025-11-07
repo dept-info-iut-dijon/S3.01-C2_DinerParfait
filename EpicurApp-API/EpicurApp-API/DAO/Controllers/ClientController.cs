811e4890df3b50e8621d80fc5ba388289f5b4557
Augustin <gusstide@gmail.com>
2025-11-07 10:42:25 +0100

    avantmerge

--- EpicurApp-API/EpicurApp-API/DAO/Controllers/ClientController.cs
+++ EpicurApp-API/EpicurApp-API/DAO/Controllers/ClientController.cs
@@ -43,6 +43,16 @@ namespace EpicurApp_API.DAO.Controllers
                 return StatusCode(StatusCodes.Status500InternalServerError, "Une erreur est survenue lors de la création du client.");
             }
         }
+        /// <summary>
+        /// ///////////////////////////////////////////////////////
+        /// </summary>
+        /// <returns></returns>
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Client>>> GetClients()
+        {
+            var clients = await _context.Clients.ToListAsync();
+            return Ok(clients);
+        }
 
         [HttpGet("{id}")]
         public IActionResult GetClient(int id)
@@ -62,6 +72,11 @@ namespace EpicurApp_API.DAO.Controllers
             }
 
         }
+
+
+
+
+
 
 
     }
